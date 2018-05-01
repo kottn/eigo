046d34c72f22a16fe314891cb5f33c5f6eeb17ac
kottn <35750723+kottn@users.noreply.github.com>
2018-05-01 19:16:15 +0900

    fix bugs in install

--- install.sh
+++ install.sh
@@ -5,5 +5,6 @@ curl -LO http://www.namazu.org/~tsuchiya/sdic/data/gene95.tar.gz
 tar zxfv *.tar.gz
 find . -maxdepth 1 -type f -print0 | xargs -0 nkf --overwrite -w -Lu
 
+mkdir -p ~/bin
 ln -snfv ../share/eigo/eigo.sh ../../bin/eigo
 
